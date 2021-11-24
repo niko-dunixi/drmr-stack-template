56b9978029b5be2319ccecffb745ddab5167705f
Paul FREAKN Baker <paul-nelson-baker@users.noreply.github.com>
2021-11-24 14:40:14 -0600

    Expanding repository definition
    This includes expanding the in-memory implementation with unit-tests to
    go along with it.

--- backend/src/domain.rs
+++ backend/src/domain.rs
@@ -3,6 +3,7 @@ mod in_memory;
 
 use std::error::Error;
 
-pub trait Repository<T> {
-
+pub trait Repository<Id, Item> {
+    fn create(&mut self, item: &Item) -> Result<Id, Box<dyn Error>>;
+    fn get(&self, id: &Id) -> Result<Option<Item>, Box<dyn Error>>;
 }
--- backend/src/domain/in_memory.rs
+++ backend/src/domain/in_memory.rs
@@ -1,26 +1,59 @@
 use super::Repository;
+use std::collections::HashMap;
+use std::error::Error;
+use std::hash::Hash;
+use uuid::Uuid;
 
-pub struct InMemory<T> {
-    data: Vec<T>,
+pub struct InMemory<T: 'static + Clone> {
+    data: HashMap<String, T>,
 }
 
-impl <T: 'static> InMemory<T> {
-    pub fn new() -> Box<dyn Repository<T>> {
-        Box::new(InMemory::<T>{
-            data: vec!(),
+impl<Item: 'static + Clone> InMemory<Item> {
+    pub fn new() -> Box<dyn Repository<String, Item>> {
+        Box::new(InMemory::<Item> {
+            data: HashMap::new(),
         })
     }
 }
 
-impl <T> Repository<T> for InMemory<T> {
+impl<Item: 'static + Clone> Repository<String, Item>
+    for InMemory<Item>
+{
+    fn create(&mut self, item: &Item) -> Result<String, Box<dyn Error>> {
+        let id = Uuid::new_v4().to_hyphenated().to_string();
+        self.data.insert(id.clone(), item.clone());
+        Ok(id)
+    }
 
+    fn get(&self, id: &String) -> Result<Option<Item>, Box<dyn Error>> {
+        Ok(self.data.get(id).cloned())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use claim::{assert_ok,assert_ok_eq,assert_some,assert_some_eq};
+
     #[test]
     fn new_in_memory_repo() {
-        let _repo: Box<dyn Repository<String>> = InMemory::new();
+        let _repo: Box<dyn Repository<String, String>> = InMemory::new();
+    }
+
+    #[test]
+    fn get_is_ok_none() {
+        let repo: Box<dyn Repository<String, String>> = InMemory::new();
+        let id = String::from("my-id-foo");
+        assert_ok_eq!(repo.get(&id), None);
+    }
+
+    #[test]
+    fn get_is_ok_some() {
+        let mut repo: Box<dyn Repository<String, String>> = InMemory::new();
+        let item = String::from("Paul FREAKN Baker");
+        let create_result = repo.create(&item);
+        assert_ok!(&create_result);
+        let id = create_result.unwrap();
+        assert_ok_eq!(repo.get(&id), Some(item));
     }
 }
